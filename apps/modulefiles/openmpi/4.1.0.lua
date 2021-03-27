925b783be834473a0e949bd4c13858521f84f35b
shivunchinniah <shivun@diorb.com>
2021-03-27 21:46:58 +0200

    Delete apps/modulefiles directory

--- apps/modulefiles/openmpi/4.1.0.lua
+++
@@ -1,7 +0,0 @@
-local base=os.getenv("APPS") .. "/openmpi/rchpc_v1"
-
-append_path("LD_LIBRARY_PATH", base .. "/openmpi/lib")
-append_path("LIBRARY_PATH", base .. "/openmpi/lib")
-append_path("PATH", base .. "/openmpi/bin")
-
-load('libfabric')
