b1bb13493c158b6cb54b4ff15691b613a279cbef
mikaeeld <mikaeel.dindar.11@gmail.com>
2021-04-03 11:51:21 +0200

    HPCG but this time I cleaned the script

--- HPCG/install_hpcg_v3.1.sh
+++ HPCG/install_hpcg_v3.1.sh
@@ -1,38 +1,11 @@
 #!/bin/bash
 
-# Download and extract tarball
-
-tarball=$DOWNLOADS/hpcg-3.1.tar.gz
-
-cd $DOWNLOADS
-
-if test -f $tarball; then
-    #exists
-    echo "Tarball Exisists..."
-else
-    #doesn't then download
-    echo "Downloading tarball..."
-   
-    wget http://www.hpcg-benchmark.org/downloads/hpcg-3.1.tar.gz
-fi
-
-echo "Removing existing source files..."
-sleep 2
-rm -rv hpcg-3.1
-
-
-# extract tarball
-echo "Extracting tarball..."
-sleep 2
-tar xvaf hpcg-3.1.tar.gz
-
-cd hpcg-3.1
-
 tarball=$DOWNLOADS/hpcg-3.1.tar.gz
 download=http://www.hpcg-benchmark.org/downloads/hpcg-3.1.tar.gz
 appname=hpcg
 appversion=hpcg-3.1
 installdir=$HPCG/$appversion
+arch=M
 
 cd $DOWNLOADS
 
@@ -67,17 +40,15 @@ ml openmpi
 
 echo "Copying Makefile"
 sleep 3
-cp $HPCG/Make.M setup
+cp $HPCG/Make.$arch setup
 
 echo "Creating directory"
 sleep 3
 mkdir $installdir
 
-echo "Configur
-
-http://www.hpcg-benchmark.org/downloads/hpcg-3.1.tar.ging..."
+echo "Configuring..."
 cd $installdir
-/$DOWNLOADS/$appversion/configure M
+/$DOWNLOADS/$appversion/configure $arch
 
 echo "Building... May take a while"
 sleep 3
