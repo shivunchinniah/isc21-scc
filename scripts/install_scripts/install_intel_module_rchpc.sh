b2b1fb8a517234f1610fed8c944e3554f6bd3744
Shivun Chinniah <shivun@diorb.com>
2021-04-03 23:44:11 +0200

    added: intelpython3 in intel module install script

--- scripts/install_scripts/install_intel_module_rchpc.sh
+++ scripts/install_scripts/install_intel_module_rchpc.sh
@@ -6,15 +6,20 @@
 file_name=2020.4.912.lua
 file_path=modulefiles/intel
 
+intelpython3=3.7.7.lua
+intelpython3_path=modulefiles/intelpython3
+
 # if test -f $APPS/$file_path/$filename
 # then
 #     echo "Module file alreaddy added."
 
 # else
 mkdir --parent $APPS/$file_path
+mkdir --parent $APPS/$intelpython3_path
 cp $SCRIPTS/$file_path/$file_name $APPS/$file_path
-echo "Module file copied!"
+cp $SCRIPTS/$intelpython3_path/$intelpython3 $APPS/$intelpython3_path
+echo "Module files copied!"
 
 # update ml cache
-ml --ignore_cache avail 
+ml --ignore_cache avail > /dev/null 2>&1
 # fi
