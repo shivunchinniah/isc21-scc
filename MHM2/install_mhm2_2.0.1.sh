ca1364cc3c4edcac927a40550a727256e7a3c8e5
mikaeeld <mikaeel.dindar.11@gmail.com>
2021-04-21 16:57:26 +0200

    Complete (hopefully) mhm script

--- MHM2/install_mhm2_2.0.1.sh
+++ MHM2/install_mhm2_2.0.1.sh
@@ -3,7 +3,7 @@
 # Download and extract tarball
 
 tarball=$DOWNLOADS/mhm2-2.0.1.tar.gz
-download=https://bitbucket.org/berkeleylab/upcxx/downloads/upcxx-2020.3.2.tar.gz
+download=https://bitbucket.org/berkeleylab/mhm2/downloads/mhm2-2.0.1.tar.gz
 appname=mhm2
 appversion=mhm2-2.0.1
 installdir=$MHM2/$appversion
@@ -40,28 +40,18 @@ echo "Loading Dependancies..."
 ml gcc
 ml openmpi
 ml upcxx
+ml cmake
 
 echo "Configuring..."
-./configure --prefix=$installdir --with-cc=mpicc --with-cxx=mpicxx
-#  --with-default-network=ibv needs to be added
+export CXX=mpicxx
+export CC=mpicc
+export MHM2_INSTALL_PATH=$installdir
 
 echo "Building... May take a while"
 sleep 3
 
 #Build
-gmake -j all
-
-echo "Installing..."
-sleep 2
-
-gmake install
-
-echo "Adding Module File"
-mkdir --parents $APPS/modulefiles/upcxx
-rm $APPS/upcxx/2020.3.2
-cp $installdir/share/modulefiles/upcxx/2020.3.2 $APPS/modulefiles/upcxx/
-# update ml cache
-module --ignore_cache avail > /dev/null
+./build.sh Release
 
 echo "DONE!!!"
 
